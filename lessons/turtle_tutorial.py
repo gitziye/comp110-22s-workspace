6b5c7f7d276db169c92e7712bac33d38d61b07b3
ziye zhou <ziye.zhou@unc.edu>
2022-02-16 16:21:01 -0500

    tutle

--- lessons/turtle_tutorial.py
+++ lessons/turtle_tutorial.py
@@ -6,11 +6,19 @@ bob.penup()
 bob.goto(45, 60)
 bob.pendown()
 
-bob.speed(200)
+bob.speed(50)
 
 i: int = 0
 while (i < 3):
     bob.forward(300)
+    bob.left(120)
+    i = i + 1
+
+side_length: int = 300
+
+i: int = 0
+while (i < 3):
+    bob.forward(side_length)
     bob.left(120)
     i = i + 1
 
