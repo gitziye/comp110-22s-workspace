e5b80cdd682aaf1ed884f58191d4c2b6bdd484e4
ziye zhou <ziye.zhou@unc.edu>
2022-02-23 14:06:43 -0500

    ex05

--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -2,3 +2,10 @@
 
 __author__ = "730528622"
 
+
+from utils import only_evens, sub, concat
+
+def test_only_evens() -> None:
+    listing = [1,2,3,4,5,6,7,8]
+    assert only_evens(listing) == [2,4,6,8]
+
