503e73326010794c5db9fda1a55eb9043e9b82fb
ziye zhou <ziye.zhou@unc.edu>
2022-03-02 12:42:26 -0500

    ex06

---
+++ exercises/ex06/dictionary.py
@@ -0,0 +1,9 @@
+""""This python file is mean to finish my COMP 110 exercise 06 homeowrk."""
+
+__author__ = "730528622"
+
+def invert(a: dict[str, str]) -> dict[str, str]:
+    
+
+    return a
+    
